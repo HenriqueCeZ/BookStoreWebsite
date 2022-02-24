443bf040cba929bec61fa10faaaf4d9d3178730a
HenriqueCeZ <henriquecezar.jp@gmail.com>
2022-02-24 15:26:57 -0300

    refactor: desacoplando o código

--- src/main/java/com/bookstore/service/UserServices.java
+++ src/main/java/com/bookstore/service/UserServices.java
@@ -18,19 +18,27 @@ public class UserServices {
 
 	private EntityManagerFactory entityManagerFactory;
 	private EntityManager entityManager;
-
 	private UserDAO userDAO;
+	private HttpServletRequest request;
+	private HttpServletResponse response;
 
-	public UserServices() {
-
+	public UserServices(HttpServletRequest request, HttpServletResponse response) {
+		this.request = request;
+		this.response = response;
 		entityManagerFactory = Persistence.createEntityManagerFactory("BookStoreWebsite");
 		entityManager = entityManagerFactory.createEntityManager();
 		userDAO = new UserDAO(entityManager);
 
 	}
 
-	public void listUser(HttpServletRequest request, HttpServletResponse response, String message)
+	public void listUser()
 			throws ServletException, IOException {
+
+		listUser(null);
+
+	}
+
+	public void listUser(String message) throws ServletException, IOException {
 		List<Users> listUsers = userDAO.listAll();
 
 		request.setAttribute("listUsers", listUsers);
@@ -45,7 +53,7 @@ public class UserServices {
 
 	}
 
-	public void createUser(HttpServletRequest request, HttpServletResponse response) {
+	public void createUser() {
 
 		String email = request.getParameter("email");
 		String fullname = request.getParameter("fullname");
